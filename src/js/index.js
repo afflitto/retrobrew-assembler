ba2ef420d5a60e480404f7ebe7e57622d29ac19e
LegoMaster616 <andrew.afflitto@gmail.com>
2019-04-02 13:42:51 -0400

    show memory

--- src/js/index.js
+++ src/js/index.js
@@ -12,5 +12,23 @@ const emulator = new Emulator({debug: false, uiTick: () => {
   $('#register-ir').text(window.emulator.ir.instruction.toString(2).padStart(4, '0') + ' ' + window.emulator.ir.data.toString(2).padStart(4, '0'));
   $('#register-flag-zero').text(window.emulator.flags.zero);
   $('#register-flag-carry').text(window.emulator.flags.carry);
+
+
+  $('#memory-display').html(generateMemoryDisplay(window.emulator.memory.memory, window.emulator.pc.value));
 }});
 window.emulator = emulator;
+
+
+function generateMemoryDisplay(memory, pc) {
+  let html = '';
+
+  for(let i = 0; i < 4; i++) {
+    html += `<div class="row">`;
+    for(let j = 0; j < 4; j++) {
+      const isPC = pc === j + i*4;
+      html += `<div class="col-xs-3 m-3 memory-display ${isPC ? 'memory-pc':''}">${memory[j + i*4].toString(16).padStart(2, '0')}</div>`;
+    }
+    html += `</div>`;
+  }
+  return html;
+}
